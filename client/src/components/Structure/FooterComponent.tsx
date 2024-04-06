63f7c1340fb1d18170f9db4bdc3ebce0828a707b
Орлик Александр Алекса <AOrlik@alfabank.ru>
2024-04-07 02:24:37 +1000

    Вынос

--- client/src/components/Structure/FooterComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {Layout} from "antd";
-const {Footer} = Layout;
-export const FooterComponent = () => {
-    return <Footer style={{textAlign: 'center'}}>
-        Создано и создано... ©{new Date().getFullYear()} Created by RadFoxDV
-    </Footer>
-}
--- client/src/tools/interfaces/localStorageInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface TokenInfo {
-    accessToken: string | null
-    refreshToken: string | null
-    entryTime: string | null
-    expiresIn: string | null
-}
